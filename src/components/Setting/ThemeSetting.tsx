444b37cd34afbdf4ca09b336a445caad94b6ac4a
Gia Minh Hoang <giaminh5567@gmail.com>
2025-01-04 14:03:19 +0700

    Update image width

--- src/components/Setting/ThemeSetting.tsx
+++ src/components/Setting/ThemeSetting.tsx
@@ -5,40 +5,49 @@ import { useAppState } from '../../context/GeneralSettings';
 //@ts-ignore
 import styles from './index.module.scss';
 interface ThemeSettingProps {
-    // Define your component props here
+  // Define your component props here
 }
 
 const isMobile = window.innerWidth < 640;
 
 const ThemeSetting: React.FC = () => {
-    // Implement your component logic here
-    const [theme, setTheme] = React.useState(0);
-    const { currentSetting, changeBackground } = useAppState();
+  // Implement your component logic here
+  const [theme, setTheme] = React.useState(0);
+  const { currentSetting, changeBackground } = useAppState();
 
-    return (
-       
-        // JSX code for your component's UI
-        <div className={styles['setting-options']}>
-              <h3>Chose theme</h3>
-              <div className={styles['theme-list']}>
-                {ThemeImages.map((element, index) => (
-                  <div
-                    key={element.id}
-                    className={styles['image-container']}
-                    onClick={() => {
-                      setTheme(element.id);
-                      changeBackground(isMobile, !isMobile ? element.desktopUrl : element.mobileUrl, element.id);
-                    }}
-                  >
-                    <img alt='bg-image'  style={{
-                      border:
-                        currentSetting.theme_index === element.id ? '3px solid var(--green-400)' : 'none',
-                    }} src={!isMobile ? element.desktopUrl : element.mobileUrl  }></img>
-                  </div>
-                ))}
-              </div>
-            </div>
-    );
+  return (
+    // JSX code for your component's UI
+    <div className={styles['setting-options']}>
+      <h3>Chose theme</h3>
+      <div className={styles['theme-list']}>
+        {ThemeImages.map((element, index) => (
+          <div
+            key={element.id}
+            className={styles['image-container']}
+            onClick={() => {
+              setTheme(element.id);
+              changeBackground(
+                isMobile,
+                !isMobile ? element.desktopUrl : element.mobileUrl,
+                element.id
+              );
+            }}
+          >
+            <img
+              alt='bg-image'
+              style={{
+                border:
+                  currentSetting.theme_index === element.id
+                    ? '3px solid var(--green-400)'
+                    : 'none',
+              }}
+              src={!isMobile ? element.desktopUrl : element.mobileUrl}
+            ></img>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ThemeSetting;
--- src/global/const.ts
+++ src/global/const.ts
@@ -1,25 +1,33 @@
-import { TSetting } from "./types";
+import { TSetting } from './types';
 
 export const ThemeImages = [
   {
     id: 0,
-    desktopUrl: 'https://images.pexels.com/photos/1367192/pexels-photo-1367192.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    mobileUrl: 'https://images.pexels.com/photos/1834399/pexels-photo-1834399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    desktopUrl:
+      'https://images.pexels.com/photos/1367192/pexels-photo-1367192.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    mobileUrl:
+      'https://images.pexels.com/photos/1834399/pexels-photo-1834399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
   },
   {
     id: 1,
-    desktopUrl: 'https://images.pexels.com/photos/547114/pexels-photo-547114.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    mobileUrl: 'https://images.pexels.com/photos/6942667/pexels-photo-6942667.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    desktopUrl:
+      'https://images.pexels.com/photos/547114/pexels-photo-547114.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    mobileUrl:
+      'https://images.pexels.com/photos/6942667/pexels-photo-6942667.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
   },
   {
     id: 2,
-    desktopUrl: 'https://images.pexels.com/photos/2187456/pexels-photo-2187456.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    mobileUrl: 'https://images.pexels.com/photos/13067863/pexels-photo-13067863.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    desktopUrl:
+      'https://images.pexels.com/photos/2187456/pexels-photo-2187456.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    mobileUrl:
+      'https://images.pexels.com/photos/13067863/pexels-photo-13067863.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
   },
   {
     id: 3,
-    desktopUrl: 'https://images.pexels.com/photos/2070047/pexels-photo-2070047.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    mobileUrl: 'https://images.pexels.com/photos/1699024/pexels-photo-1699024.jpeg?auto=compress&cs=tinysrgb&w=800',
+    desktopUrl:
+      'https://images.pexels.com/photos/2070047/pexels-photo-2070047.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    mobileUrl:
+      'https://images.pexels.com/photos/1699024/pexels-photo-1699024.jpeg?auto=compress&cs=tinysrgb&w=800',
   },
 ];
 
@@ -47,16 +55,16 @@ export const AlarmSounds = [
 ];
 
 export enum SettingTypes {
-    THEME = 'Theme',
-    NOTIFICATION_SOUNDS = 'Notification Sounds',
-    VOLUME = 'Volume',
-    TIMER_LENGTH = 'Timer Length',
-    POMO_TIME = 'Pomodoro Time',
-    SHORT_BREAK_TIME = 'Short Break Time',
-    LONG_BREAK_TIME = 'Long Break Time',
-    FOCUS_LEVEL = 'Focus Level',
-    ACCOUNT = 'Account',
-    MODE = 'Mode',
+  THEME = 'Theme',
+  NOTIFICATION_SOUNDS = 'Notification Sounds',
+  VOLUME = 'Volume',
+  TIMER_LENGTH = 'Timer Length',
+  POMO_TIME = 'Pomodoro Time',
+  SHORT_BREAK_TIME = 'Short Break Time',
+  LONG_BREAK_TIME = 'Long Break Time',
+  FOCUS_LEVEL = 'Focus Level',
+  ACCOUNT = 'Account',
+  MODE = 'Mode',
 }
 
 export enum Modes {
@@ -66,19 +74,21 @@ export enum Modes {
 }
 
 export const defaultSetting: TSetting = {
-    desktop_background: 'https://images.pexels.com/photos/1367192/pexels-photo-1367192.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    mobile_background: 'https://images.pexels.com/photos/1834399/pexels-photo-1834399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    alarm: 0,
-    volume: 50,
-    timer_length: 1500,
-    pomo_time: 1500,
-    short_break_time: 300,
-    long_break_time: 600,
-    mode: Modes.POMODORO,
-    theme_index: 0,
-}
+  desktop_background:
+    'https://images.pexels.com/photos/1367192/pexels-photo-1367192.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  mobile_background:
+    'https://images.pexels.com/photos/1834399/pexels-photo-1834399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  alarm: 0,
+  volume: 50,
+  timer_length: 10,
+  pomo_time: 10,
+  short_break_time: 300,
+  long_break_time: 600,
+  mode: Modes.POMODORO,
+  theme_index: 0,
+};
 
 export enum ClockTypes {
-    DIGITAL = 'Digital',
-    ANALOG = 'Analog'
+  DIGITAL = 'Digital',
+  ANALOG = 'Analog',
 }
